62b8b782f80f1f6927dd0162af61fbc3693d7d91
Siriron <127705750+Siriron@users.noreply.github.com>
2025-12-14 08:50:51 +0600

    Create head.tsx

---
+++ app/head.tsx
@@ -0,0 +1,10 @@
+// app/head.tsx
+export default function Head() {
+  return (
+    <>
+      <title>My Mini App</title>
+      <meta name="base:app_id" content="693e229dd77c069a945bdea7" />
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    </>
+  )
+}
